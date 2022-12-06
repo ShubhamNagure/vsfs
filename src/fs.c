24d7e5daf02e7a07da111543e90e996f00f332a4
Jay Bhavsar <jaybhavsar629@gmail.com>
2022-12-06 11:00:46 +0000

    commit

--- src/fs.c
+++ src/fs.c
@@ -1,11 +1,14 @@
-#include<fs.h>
+#include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
+#include "fs.h"
+
 // void create_fs(); //to intialize new FS
 // void mount_fs();    // load FS
 // void sync_fs();     //write the FS
 
 struct superblock sb;
-struct inodes *inodes; //pointer variable to point to blocks
+struct inode *inodes; //pointer variable to point to blocks
 struct disk_block *dbs; 
 
 //to initilaize new Fs
@@ -16,20 +19,60 @@ void create_fs(){
 
     //setup inodes and disk blocks
     int i;
-    inodes= malloc(sizeof ( struct inodes )* sb.num_inodes); //inodes
+    inodes= malloc(sizeof ( struct inode)* sb.num_inodes); //inodes
     for (i=0; i< sb.num_inodes; i++)
     {
         inodes[i].size=-1;
-        inodes[i].name="";
+        strcpy(inodes[i].name,"makefile");
 
     } //init inodes
 
-    dbs=malloc(sizeof(struct disk_blocks)* sb.num_blocks); //disk block 
-    for (i=0; i< sb.num_blocks; i++)
+    dbs=malloc(sizeof (struct disk_block)* sb.num_blocks); //disk block 
+    /*what we want is that allocate the space for the disk so to achieve 
+    that we malloc() and in that we cal the size of the disk block struct which 
+    is multiple by the number of the blocks that are associated for the data block by 
+    superblock
+    */
+    for (i=0; i< sb.num_blocks; i++) 
     {
-        inodes[i].next_block_num=-1;
+          dbs[i].next_block_num=-1;
+    }//init dbs
+    
+
+
+}// create fs
+
+//load the Fs for that we have to mount it.
+void mount_fs(){
+
+     
+} 
+
+
+//to write something into Fs we need to sync Fs
+void sync_fs(){
+    FILE *file;
+    file=fopen("fs_data","w+");
+
+    //superblock
+    fwrite(&sb, sizeof(struct superblock),1,file);
+
+    //inodes 
+    int i;
+    for (i=0; i< sb.num_inodes; i++)
+    {
+        fwrite(&(inodes[i]), sizeof(struct inode),1,file);
         
 
-    }
+    } //write inodes
+    
 
-}
+    for (i=0; i< sb.num_blocks ; i++)
+    {
+        fwrite(&(dbs[i]), sizeof(struct disk_block),1,file);
+        
+
+    }//write data_blocks
+    fclose(file);
+}//sync_fs 
+
---
+++ src/test_file.c
@@ -0,0 +1,12 @@
+#include "fs.h"
+#include<stdio.h>
+
+
+void main()
+{
+    create_fs();
+    sync_fs();
+    printf("done");
+
+
+}//main
