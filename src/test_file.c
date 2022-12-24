a12366b6823df05f5d052b25d690117071fbe693
Jay Bhavsar <jaybhavsar629@gmail.com>
2022-12-24 17:09:39 +0000

    creating the file1 and writing the data.

--- src/test_file.c
+++ src/test_file.c
@@ -5,18 +5,18 @@ void main()
     // create_fs();
    
     mount_fs();
-    // int file=allocate_file("file");
-    // set_filesize(file,100);
+    int file=allocate_file("file1");
+    set_filesize(file,500);
    
     print_fs();
-    // int i;
-    // char data='j';
-    // for ( i = 0; i <49; i++){
-    //     write_byte(file ,i*100,&data);
+    int i;
+    char data='b';
+    for ( i = 0; i <49; i++){
+        write_byte(file ,i*100,&data);
 
-    // }
-    //to read the data
-    //  printf("file%d",file);
+    }
+   // to read the data
+    // printf("file%d",file);
     sync_fs();
     // read_byte(1);
     printf("done\n");
